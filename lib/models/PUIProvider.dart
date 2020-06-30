d25be070b70c95ca0534d8be77c3db4cfe305468
Bernard Gresola <gresola_bernard@yahoo.com>
2020-06-30 15:13:14 +0800

    For PUI Provider

--- lib/models/PUIProvider.dart
+++ lib/models/PUIProvider.dart
@@ -1,10 +1,135 @@
 import 'dart:convert';
+
 import 'package:covid_monitoring/services/Api.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:http/http.dart' as http;
+
+List<Map> inputs = [
+      {
+        'type': FormBuilderDateTimePicker,
+        'attribute': 'date_of_testing',
+        'label': 'Date of Testing',
+        'inputType': InputType.date,
+        'firstDate': DateTime.now(),
+        'validators': [FormBuilderValidators.required()],
+      },
+      {
+        'type': FormBuilderTextField,
+        'attribute': 'surname',
+        'label': 'Surname',
+        'validators': [FormBuilderValidators.required()],
+      },
+      {
+        'type': FormBuilderTextField,
+        'attribute': 'firstname',
+        'label': 'Firstname',
+        'validators': [FormBuilderValidators.required()],
+      },
+      {
+        'type': FormBuilderTextField,
+        'attribute': 'id_no',
+        'label': 'ID No.',
+        'validators': [FormBuilderValidators.required()],
+      },
+      {
+        'type': FormBuilderTextField,
+        'attribute': 'contact_no',
+        'label': 'Contact No.',
+        'validators': [FormBuilderValidators.required()],
+      },
+      {
+        'type': FormBuilderDropdown,
+        'attribute': 'specimen_result',
+        'label': 'Specimen Result',
+        'hint': 'Select Specimen Result',
+        'selections': ['Not Detected', 'Detected'],
+        'validators': [FormBuilderValidators.required()],
+      },
+      {
+        'type': FormBuilderDropdown,
+        'attribute': 'test_result',
+        'label': 'Test Result',
+        'hint': 'Select Test Result',
+        'selections': ['Covid (-)', 'Covid (+)'],
+        'validators': [FormBuilderValidators.required()],
+      },
+      {
+        'type': FormBuilderTextField,
+        'attribute': 'classification',
+        'label': 'Classification',
+        'validators': [],
+      },
+      {
+        'type': FormBuilderDropdown,
+        'attribute': 'follow_up_test',
+        'label': 'Follow Up Test',
+        'hint': 'Select for Follow Up Test',
+        'selections': ['Yes', 'No'],
+        'validators': [],
+      },
+      {
+        'type': FormBuilderDropdown,
+        'attribute': 'admission',
+        'label': 'Admitted',
+        'hint': 'Select for Admission',
+        'selections': ['Admitted', 'Not Admitted'],
+        'validators': [],
+      },
+      {
+        'type': FormBuilderTextField,
+        'attribute': 'ward',
+        'label': 'Ward',
+        'validators': [],
+      },
+      {
+        'type': FormBuilderTextField,
+        'attribute': 'outcome_after_admission',
+        'label': 'Outcome After Admission',
+        'validators': [],
+      },
+      {
+        'type': FormBuilderTextField,
+        'attribute': 'other_outcome',
+        'label': 'Other Outcome',
+        'validators': [],
+      },
+      {
+        'type': FormBuilderDateTimePicker,
+        'attribute': 'date_of_admission',
+        'label': 'Date of Admission',
+        'inputType': InputType.date,
+        'firstDate': DateTime.now(),
+        'validators': [],
+      },
+      {
+        'type': FormBuilderDateTimePicker,
+        'attribute': 'date_of_outcome',
+        'label': 'Date of Outcome',
+        'inputType': InputType.date,
+        'firstDate': DateTime.now(),
+        'validators': [],
+      },
+      {
+        'type': FormBuilderTextField,
+        'attribute': 'remarks',
+        'label': 'Remarks',
+        'maxLines': 3,
+        'validators': [],
+      },
+      {
+        'type': FormBuilderTextField,
+        'attribute': 'other_remarks',
+        'label': 'Other Remarks',
+        'maxLines': 3,
+        'validators': [],
+      },
+    ];
 
 class PUIProvider extends ChangeNotifier
 {
+  
+
 
   savePUIForm(Map form) async {
 
--- lib/screens/forms/PUIForm.dart
+++ lib/screens/forms/PUIForm.dart
@@ -1,3 +1,4 @@
+import 'package:covid_monitoring/models/PUIProvider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
@@ -13,147 +14,18 @@ class PUIForm extends StatefulWidget {
 class _PUIFormState extends State<PUIForm> {
   final GlobalKey<FormBuilderState> _formKey = GlobalKey<FormBuilderState>();
 
- 
-
   @override
   Widget build(BuildContext context) {
-
-    List<Map> inputs = [
-      {
-        'type': FormBuilderDateTimePicker,
-        'attribute': 'date_of_testing',
-        'label': 'Date of Testing',
-        'inputType': InputType.date,
-        'firstDate': DateTime.now(),
-        'required': FormBuilderValidators.required(),
-      },
-      
-      {
-        'type': FormBuilderTextField,
-        'attribute': 'surname',
-        'label': 'Surname',
-        'required': FormBuilderValidators.required(),
-      },
-
-      {
-        'type': FormBuilderTextField,
-        'attribute': 'firstname',
-        'label': 'Firstname',
-        'required': FormBuilderValidators.required(),
-      },
-
-      {
-        'type': FormBuilderTextField,
-        'attribute': 'id_no',
-        'label': 'ID No.',
-        'required': FormBuilderValidators.required(),
-      },
-
-      {
-        'type': FormBuilderTextField,
-        'attribute': 'contact_no',
-        'label': 'Contact No.',
-        'required': FormBuilderValidators.required(),
-      },
-
-      {
-        'type': FormBuilderDropdown,
-        'attribute': 'specimen_result',
-        'label': 'Specimen Result',
-        'hint': 'Select Specimen Result',
-        'selections': ['Not Detected', 'Detected'],
-        'required': FormBuilderValidators.required(),
-      },
-
-      {
-        'type': FormBuilderDropdown,
-        'attribute': 'test_result',
-        'label': 'Test Result',
-        'hint': 'Select Test Result',
-        'selections': ['Covid (-)', 'Covid (+)'],
-        'required': FormBuilderValidators.required(),
-      },
-
-      {
-        'type': FormBuilderTextField,
-        'attribute': 'classification',
-        'label': 'Classification',
-      },
-
-      {
-        'type': FormBuilderDropdown,
-        'attribute': 'follow_up_test',
-        'label': 'Follow Up Test',
-        'hint': 'Select for Follow Up Test',
-        'selections': ['Yes', 'No'],
-      },
-
-      {
-        'type': FormBuilderDropdown,
-        'attribute': 'admission',
-        'label': 'Admitted',
-        'hint': 'Select for Admission',
-        'selections': ['Admitted', 'Not Admitted'],
-      },
-
-      {
-        'type': FormBuilderTextField,
-        'attribute': 'ward',
-        'label': 'Ward',
-      },
-
-      {
-        'type': FormBuilderTextField,
-        'attribute': 'outcome_after_admission',
-        'label': 'Outcome After Admission',
-      },
-
-      {
-        'type': FormBuilderTextField,
-        'attribute': 'other_outcome',
-        'label': 'Other Outcome',
-      },
-
-      {
-        'type': FormBuilderDateTimePicker,
-        'attribute': 'date_of_admission',
-        'label': 'Date of Admission',
-        'inputType': InputType.date,
-        'firstDate': DateTime.now(),
-      },
-
-      {
-        'type': FormBuilderDateTimePicker,
-        'attribute': 'date_of_outcome',
-        'label': 'Date of Outcome',
-        'inputType': InputType.date,
-        'firstDate': DateTime.now(),
-      },
-
-      {
-        'type': FormBuilderTextField,
-        'attribute': 'remarks',
-        'label': 'Remarks',
-        'maxLines': 3,
-      },
-
-      {
-        'type': FormBuilderTextField,
-        'attribute': 'other_remarks',
-        'label': 'Other Remarks',
-        'maxLines': 3,
-      },
-      
-    ];
+    
 
     _savePUIForm({BuildContext context}) async {
       if (_formKey.currentState.saveAndValidate()) {
         var formVals = _formKey.currentState.value;
-        
+
         print(formVals);
       }
     }
-    
+
     return FormBuilder(
         key: _formKey,
         autovalidate: true,
@@ -162,78 +34,70 @@ class _PUIFormState extends State<PUIForm> {
           child: SingleChildScrollView(
             child: Column(
               children: [
-                  Container(
-                    padding: EdgeInsets.all(20),
-                    margin: EdgeInsets.all(10),
-                    color: Colors.blue[100],
-                    child: Text(
-                      'Please fill up all the fields',
+                Container(
+                  padding: EdgeInsets.all(20),
+                  margin: EdgeInsets.all(10),
+                  color: Colors.blue[100],
+                  child: Text('Please fill up all the fields',
                       style: Theme.of(context).textTheme.bodyText2.copyWith(
-                        color: Colors.blue[900],
-                      )
-                    ),
+                            color: Colors.blue[900],
+                          )),
+                ),
+                ...inputs.map((item) {
+                  switch (item['type']) {
+                    case FormBuilderTextField:
+                      // print(item['required'].runtimeType);
+                      return FormBuilderTextField(
+                        attribute: item['attribute'],
+                        validators: [...item['validators']],
+                        keyboardType: TextInputType.text,
+                        decoration: InputDecoration(labelText: item['label']),
+                      );
+                      break;
+                    case FormBuilderDateTimePicker:
+                      // print(item['validators'].runtimeType);
+                      return FormBuilderDateTimePicker(
+                        attribute: item['attribute'],
+                        inputType: item['inputType'],
+                        firstDate: item['firstDate'],
+                        decoration: InputDecoration(labelText: item['label']),
+                        validators: [...item['validators']],
+                      );
+                    case FormBuilderDropdown:
+                      return FormBuilderDropdown(
+                          attribute: item['attribute'],
+                          decoration: InputDecoration(
+                            labelText: item['label'],
+                          ),
+                          // initialValue: 'Male',
+                          hint: Text(item['hint']),
+                          validators: [
+                            ...item['validators']
+                          ],
+                          items: [
+                            ...item['selections']
+                                .map((items) => DropdownMenuItem(
+                                    value: items, child: Text("$items")))
+                                .toList(),
+                          ]);
+
+                    default:
+                      return Text('');
+                  }
+                }).toList(),
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: RaisedButton.icon(
+                    onPressed: () {
+                      _savePUIForm();
+                    },
+                    icon: Icon(FontAwesomeIcons.save),
+                    label: Text('Save'),
                   ),
-                  ...inputs.map((item) {
-                    switch (item['type']) {
-                      case FormBuilderTextField:
-                        return 
-                          FormBuilderTextField(
-                            attribute: item['attribute'],
-                            validators: [
-                              item['required'] ? item['required'] : ''
-                            ],
-                            keyboardType: TextInputType.text,
-                            decoration: InputDecoration(labelText: item['label']),
-                          );
-                        break;
-                      case FormBuilderDateTimePicker:
-                        return 
-                          FormBuilderDateTimePicker(
-                            attribute: item['attribute'],
-                            inputType: item['inputType'],
-                            firstDate: item['firstDate'],
-                            decoration: InputDecoration(labelText: item['label']),
-                            validators: [item['required']],
-                          );
-                      case FormBuilderDropdown:
-                        return 
-                          
-                          FormBuilderDropdown(
-                            attribute: item['attribute'],
-                            decoration: InputDecoration(labelText: item['label'],),
-                            // initialValue: 'Male',
-                            hint: Text(item['hint']),
-                            validators: [item['required']],
-                            items: [
-                              ...item['selections'].map((items) => 
-                              DropdownMenuItem(
-                                  value: items,
-                                  child: Text("$items")
-                              )).toList(),
-                            ]
-                          );
-
-                      default:
-                        return 
-                          Text('');
-                    }
-                  }).toList(),
-
-
-                  Padding(
-                    padding: const EdgeInsets.all(20.0),
-                    child: RaisedButton.icon(
-                      onPressed: () {
-                        _savePUIForm();
-                      },
-                      icon: Icon(FontAwesomeIcons.save),
-                      label: Text('Save'),
-                    ),
-                  ),
+                ),
               ],
             ),
           ),
-        )
-      );
+        ));
   }
 }
